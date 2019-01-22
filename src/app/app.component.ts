5c0a1462cfcdb520dfd26db53d9504b8b086b4ff
Chenguitti <omar.chengui@gmail.com>
2019-01-22 14:07:17 +0100

    router

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,41 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-import { AppareilService } from './services/appareil.service';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
-  isAuth = false;
-  lastUpdate = new Promise((resolve, reject) => {
-    const date = new Date();
-    setTimeout(
-      () => {
-        resolve(date);
-      }, 2000
-    );
-  });
-  appareils:any [] ;
-  
-  ngOnInit(){
-    this.appareils=this.appareilService.appareils;
-  }
- 
-  onAllumer() {
-    this.appareilService.switchOnAll();
-}
-  onEteindre(){
-    this.appareilService.switchOffAll();
-  }
-  constructor(private appareilService : AppareilService) {
-    setTimeout(
-      () => {
-        this.isAuth = true;
-      }, 1000
-    );
-  }
+export class AppComponent {
 
+constructor(){
+  
+}
   
  
 }
